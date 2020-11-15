3a5cc9bd6203187fd0d3a7f469ded728e09db097
yathavan <yadhavanprabhakar744@gmail.com>
2020-11-15 12:47:28 +0530

    struggle

--- js/main.js
+++ js/main.js
@@ -7,27 +7,13 @@ function accordionFn(){
     const button = section.querySelectorAll('button');
     //get title
      const title = section.querySelectorAll('h2');
+     //get answer
+     const answer = document.querySelectorAll('p');
+     const answerArr = [...answer];
+
+     console.log(answerArr.length);
 
 
-
-function showFn(e) {
-    e.preventDefault();
-    const answer = document.querySelectorAll('p');
- 
-
-
-    //get parent 
-    const parent = e.target.parentElement;
-    const para = parent.querySelector('p');
-   
-   
-  
-   para.classList.toggle('show');
-
-    
-    
-    
-}
 
    
 
@@ -36,8 +22,61 @@ function showFn(e) {
 
 
     //events
-    button.forEach((icon)=>icon.addEventListener('click', showFn));
-    title.forEach((title=>title.addEventListener('click', showFn)));
+    button.forEach((icon, index)=>icon.addEventListener('click',(e)=>{
+        e.preventDefault();
+       
+        answerArr.forEach((answer)=>{
+           if (index== 0) {
+               answerArr[index].classList.toggle('show')
+               console.log(index);
+              
+               
+           }else if (index == answerArr.length-2) {
+           
+            answerArr[index].classList.toggle('show')
+            answerArr[index-1].classList.remove('show');
+            answerArr[answerArr.length-1].classList.remove('show');
+          
+           }
+           else{
+               answerArr[index-1].classList.remove('show');
+            
+             
+            answerArr[index].classList.toggle('show')
+           }
+          
+        })
+
+       
+    }));
+    title.forEach((title, index)=>title.addEventListener('click',(e)=>{
+        e.preventDefault();
+       
+        answerArr.forEach((answer)=>{
+           if (index== 0) {
+               answerArr[index].classList.toggle('show')
+               console.log(index);
+              
+               
+           }else if (index == answerArr.length-2) {
+           
+            answerArr[index].classList.toggle('show')
+            answerArr[index-1].classList.remove('show');
+            
+            answerArr[answerArr.length-1].classList.remove('show');
+          
+           }
+           else{
+               answerArr[index-1].classList.remove('show');
+            
+             
+            answerArr[index].classList.toggle('show')
+           }
+          
+        })
+
+       
+    }));
 
 
 }
