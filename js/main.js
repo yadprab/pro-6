8ee1c3c27b1e1ceefee45e126f4b12b972bc4c2b
yadprab <68110471+yadprab@users.noreply.github.com>
2020-12-13 12:00:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,97 +1,60 @@
+function accordionFn() {
+  //get section
+  const section = document.querySelector(".answer--section");
+  //get button
+  const button = section.querySelectorAll("button");
+  //get title
+  const title = section.querySelectorAll("h2");
+  //get answer
+  const answer = document.querySelectorAll("p");
+  const answerArr = [...answer];
 
-function accordionFn(){
+  console.log(answerArr.length);
 
-    //get section
-    const section = document.querySelector('.answer--section');
-    //get button
-    const button = section.querySelectorAll('button');
-    //get title
-     const title = section.querySelectorAll('h2');
-     //get answer
-     const answer = document.querySelectorAll('p');
-     const answerArr = [...answer];
+  //events
+  button.forEach((icon, index) =>
+    icon.addEventListener("click", (e) => {
+      e.preventDefault();
 
-     console.log(answerArr.length);
+      answerArr.forEach((answer) => {
+        if (index == 0) {
+          answerArr[index].classList.toggle("show");
+          console.log(index);
+        } else if (index == answerArr.length - 2) {
+          answerArr[index].classList.toggle("show");
+          answerArr[index - 1].classList.remove("show");
+          answerArr[answerArr.length - 1].classList.remove("show");
+        } else {
+          answerArr[index - 1].classList.remove("show");
 
+          answerArr[index].classList.toggle("show");
+        }
+      });
+    })
+  );
+  title.forEach((title, index) =>
+    title.addEventListener("click", (e) => {
+      e.preventDefault();
 
+      answerArr.forEach((answer) => {
+        if (index == 0) {
+          answerArr[index].classList.toggle("show");
+          console.log(index);
+        } else if (index == answerArr.length - 2) {
+          answerArr[index].classList.toggle("show");
+          answerArr[index - 1].classList.remove("show");
 
-   
+          answerArr[answerArr.length - 1].classList.remove("show");
+        } else {
+          answerArr[index - 1].classList.remove("show");
 
-
-
-
-
-    //events
-    button.forEach((icon, index)=>icon.addEventListener('click',(e)=>{
-        e.preventDefault();
-       
-        answerArr.forEach((answer)=>{
-           if (index== 0) {
-               answerArr[index].classList.toggle('show')
-               console.log(index);
-              
-               
-           }else if (index == answerArr.length-2) {
-           
-            answerArr[index].classList.toggle('show')
-            answerArr[index-1].classList.remove('show');
-            answerArr[answerArr.length-1].classList.remove('show');
-          
-           }
-           else{
-               answerArr[index-1].classList.remove('show');
-            
-             
-            answerArr[index].classList.toggle('show')
-           }
-          
-        })
-
-       
-    }));
-    title.forEach((title, index)=>title.addEventListener('click',(e)=>{
-        e.preventDefault();
-       
-        answerArr.forEach((answer)=>{
-           if (index== 0) {
-               answerArr[index].classList.toggle('show')
-               console.log(index);
-              
-               
-           }else if (index == answerArr.length-2) {
-           
-            answerArr[index].classList.toggle('show')
-            answerArr[index-1].classList.remove('show');
-            
-            answerArr[answerArr.length-1].classList.remove('show');
-          
-           }
-           else{
-               answerArr[index-1].classList.remove('show');
-            
-             
-            answerArr[index].classList.toggle('show')
-           }
-          
-        })
-
-       
-    }));
-
-
+          answerArr[index].classList.toggle("show");
+        }
+      });
+    })
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
 
 //window-event
 
-window.addEventListener('DOMContentLoaded', accordionFn)
+window.addEventListener("DOMContentLoaded", accordionFn);
